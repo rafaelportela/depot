7ab3ffec131b90a23e7b04d05860cac81abc84fa
rafaelportela <rafaelportela90@gmail.com>
2012-02-07 16:23:24 -0200

    Revert "commits que não me lembro."
    This reverts commit 2ef9e59abf9f9d00692700d8460f3bb227c03072.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,5 @@
 class StoreController < ApplicationController
   def index
-    if session[:counter].nil?
-      session[:counter] = 1
-    end
-    session[:counter] = session[:counter] + 1
-    if session[:counter] > 5
-      @has_more_than_five_accesses = true
-    else
-      @has_more_than_five_accesses = false  
-    end 
     @products = Product.all
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110816024310) do
+ActiveRecord::Schema.define(:version => 20110809010249) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(:version => 20110816024310) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
