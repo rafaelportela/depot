c21c27570f7ed1d153a7b99a0c9e1ac60201d28d
rafaelportela <rafaelportela90@gmail.com>
2012-02-07 16:30:17 -0200

    modificação antes de voltar

--- db/migrate/20110816021629_add_quantity_to_line_items.rb
+++
@@ -1,9 +0,0 @@
-class AddQuantityToLineItems < ActiveRecord::Migration
-  def self.up
-    add_column :line_items, :quantity, :integer, :default => 1
-  end
-
-  def self.down
-    remove_column :line_items, :quantity
-  end
-end
