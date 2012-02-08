b65fa53455c9580c0d234ac18403f9e635a59086
rafaelportela <rafaelportela90@gmail.com>
2012-02-08 16:38:42 -0200

    iteration D playtime (index page views count)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,21 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    inc_counter
+    
+    notice = "session " + "XXX " + session[:counter].to_s
+    puts notice
   end
+  
+  
+  private
+  
+    def inc_counter
+      if session[:counter].nil?
+        session[:counter] = 1
+      else
+        session[:counter] += 1
+      end
+    end
 
 end
