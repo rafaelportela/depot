8978895b3fabedc2e8680bb276720400d7efd3b8
rafaelportela <rafaelportela90@gmail.com>
2012-02-10 15:14:40 -0200

    iteration E3: summing prices in the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -77,11 +77,13 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.xml
   def destroy
-    @cart = Cart.find(params[:id])
+    @cart = current_cart
+    #@cart = Cart.find(params[:id])
     @cart.destroy
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to(carts_url) }
+      format.html { redirect_to(store_url, :notice => 'Your cart is currentlu empty') }
       format.xml  { head :ok }
     end
   end
