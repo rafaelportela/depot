30aae2dd8a61a3a1fcee7243e00798e5245ff203
rafaelportela <rafaelportela90@gmail.com>
2012-03-16 05:12:47 -0300

    ficou erro de merge, corrigido

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Depot::Application.routes.draw do
       get :who_bought, :on => :member
     end
     root :to => "store#index", :as => 'store'
-  end  
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -69,11 +69,8 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-<<<<<<< HEAD
-
-=======
->>>>>>> iteration J1: selecting the locale 1
   
+    
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
